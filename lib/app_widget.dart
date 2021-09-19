7ea7455f818ad0df2a66044cb5f63a1f80fae93f
George Armando <georgearmando54@gmail.com>
2021-09-19 14:20:55 +0100

    feat: create app widget

---
+++ lib/app_widget.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+import 'modules/home/home_page.dart';
+import 'modules/login/login_page.dart';
+import 'modules/splash/splash_page.dart';
+import 'shared/themes/app_colors.dart';
+
+class AppWidget extends StatelessWidget {
+  const AppWidget({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Pay Flow',
+      theme: ThemeData(primaryColor: AppColors.primary),
+      initialRoute: '/splash',
+      routes: {
+        '/splash': (context) => const SplashPage(),
+        '/home': (context) => const HomePage(),
+        '/login': (context) => const LoginPage(),
+      },
+    );
+  }
+}
